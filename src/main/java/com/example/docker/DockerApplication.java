25431fc90f8423d85dd5fbfc734f8f43cad0a67d
farozy <farozy12@gmail.com>
2024-09-13 02:03:27 +0700

    add function hello

--- src/main/java/com/example/docker/DockerApplication.java
+++ src/main/java/com/example/docker/DockerApplication.java
@@ -2,9 +2,15 @@ package com.example.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DockerApplication {
+
+	 @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerApplication.class, args);
